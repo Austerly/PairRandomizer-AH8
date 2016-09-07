a010d78d8cb3b1e0b45d6970fc15f85b69bbbfd0
Taylor Mott <taylor@mottapplications.com>
2016-09-07 10:17:20 -0600

    SubjectListTableViewController
    Signed-off-by: Taylor Mott <taylor@mottapplications.com>

--- PairRandomizer/SubjectListTableViewController.swift
+++ PairRandomizer/SubjectListTableViewController.swift
@@ -20,8 +20,59 @@ class SubjectListTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // MARK: - TableView Data Source
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SubjectController.allSubjects().count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("subjectCell", forIndexPath: indexPath)
+        
+        let subject = SubjectController.allSubjects()[indexPath.row]
+        
+        cell.textLabel?.text = subject.name
+        
+        return cell
+    }
+    
+    
     @IBAction func addSubjectButtonTapped() {
         
+        let alert = UIAlertController(title: "Add New Subject", message: nil, preferredStyle: .Alert)
+        
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Subject Name"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "Add", style: .Default, handler: { (_) in
+            guard let textFields = alert.textFields,
+                subjectTextField = textFields.first else { return }
+            
+            let newSubject = SubjectController.createSubject()
+            newSubject.name = subjectTextField.text ?? "No Subject Name"
+            
+            self.tableView.reloadData()
+        }))
+        
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "toPairDetail" {
+            if let cell = sender as? UITableViewCell, indexPath = tableView.indexPathForCell(cell) {
+                let subjectToPass = SubjectController.allSubjects()[indexPath.row]
+                
+                let pairTableViewController = segue.destinationViewController as? PairTableViewController
+                
+                pairTableViewController?.subject = subjectToPass
+            }
+        }
     }
 
 
