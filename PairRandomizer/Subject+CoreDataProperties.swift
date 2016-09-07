0006d0477a3514b4262d4fb1140f6a0ccf5ca1b5
Taylor Mott <taylor@mottapplications.com>
2016-09-06 19:17:31 -0600

    Sets up core data model objects
    Signed-off-by: Taylor Mott <taylor@mottapplications.com>

---
+++ PairRandomizer/Subject+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Subject+CoreDataProperties.swift
+//  PairRandomizer
+//
+//  Created by Taylor Mott on 9/6/16.
+//  Copyright © 2016 Mott Applications. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Subject {
+
+    @NSManaged var name: String?
+    @NSManaged var teacherName: String?
+    @NSManaged var identifer: String?
+    @NSManaged var students: NSSet?
+
+}
---
+++ PairRandomizer/Subject.swift
@@ -0,0 +1,17 @@
+//
+//  Subject.swift
+//  PairRandomizer
+//
+//  Created by Taylor Mott on 9/6/16.
+//  Copyright © 2016 Mott Applications. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Subject: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
